640c055814216f7cbd3d3a9ae085a18361147533
Tobias <tobias.bp2004@gmail.com>
2025-10-29 20:48:18 -0300

    feat:  new routes added

---
+++ src/routes/workouts.ts
@@ -0,0 +1,56 @@
+import { getWorkout, getWorkouts } from '../repositories/workouts';
+import type { FastifyTypedInstance } from '../types'
+import z from "zod"
+
+export async function workoutRoutes(server: FastifyTypedInstance) {
+  server.get('/getWorkouts', {
+    schema: {
+      description: 'Get all workouts registred in the db',
+      response:{
+        200: z.object({
+          message: z.unknown()
+        }),
+        404: z.object({
+          message: z.string()
+        }),
+      }
+    }
+  }, async(request, reply) => {
+    try {
+      const response = await getWorkouts();
+
+      return reply.status(200).send({ message: response });
+    } catch (error) {
+      console.log(error);
+
+      return reply.status(200).send({ message: "Error to fetch" })      
+    }
+  });
+  
+  server.get('/getWorkout', {
+    schema: {
+      description: 'Get all workouts registred in the db',
+      querystring: z.object({
+        id: z.number(),
+      }),
+      response:{
+        200: z.object({
+          message: z.unknown()
+        }),
+        404: z.object({
+          message: z.string()
+        }),
+      }
+    }
+  }, async(request, reply) => {
+    try {
+      const response = await getWorkout(request.query.id);
+
+      return reply.status(200).send({ message: response });
+    } catch (error) {
+      console.log(error);
+
+      return reply.status(200).send({ message: "Error to fetch" })      
+    }
+  });
+}
