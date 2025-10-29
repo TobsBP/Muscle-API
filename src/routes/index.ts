3230c75ca95104209f141bc63d1e504b8ebaa352
Tobias <tobias.bp2004@gmail.com>
2025-10-29 20:47:45 -0300

    feat: added route workout

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import type { FastifyTypedInstance } from '../types'
 import { exerciseRoutes } from './exercises'
+import { workoutRoutes } from './workouts';
 
 export async function routes(server: FastifyTypedInstance) {
   server.register(exerciseRoutes);
+  server.register(workoutRoutes);
 }
